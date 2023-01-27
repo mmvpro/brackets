8ffac5d7e09f61a6b521706ce05048f06ea8fdb5
mmvpro <dublibum@gmail.com>
2023-01-27 22:40:19 +0300

    done brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const arr = str.split("");
+  const bs = {};
+  const stack = [];
+
+  bracketsConfig.forEach((e) => (bs[e[0]] = e[1]));
+
+  for (let i of arr) {
+    if (bs[i]) {
+      if (bs[i] === i && stack[stack.length - 1] === i) {
+        stack.pop();
+        continue;
+      }
+      stack.push(i);
+    } else {
+      if (!stack.length) {
+        return false;
+      }
+      if (i !== bs[stack.pop()]) return false;
+    }
+  }
+  return !stack.length;
+
 }
